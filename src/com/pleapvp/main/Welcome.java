6e65d0171c8faf618199c9e0537ff4e262ce6be1
BitBus <ejerald@email.arizona.edu>
2013-05-08 04:40:40 -0700

    Added Welcome.java - implements the Joining of a player

---
+++ src/com/pleapvp/main/Welcome.java
@@ -0,0 +1,13 @@
+package com.pleapvp.main;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public class Welcome extends JavaPlugin implements Listener {
+	@EventHandler
+	public void onJoinEvent(PlayerJoinEvent event){
+		
+	}
+}
